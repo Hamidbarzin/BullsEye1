7c2ebbc89c8b6cd1911c2fe29b3e4204181ecc1e
hamid <160804085+Hamidbarzin@users.noreply.github.com>
2024-06-02 01:20:38 -0400

    Update ViewController.swift
    Assignment2

--- iosApp1/ViewController.swift
+++ iosApp1/ViewController.swift
@@ -21,41 +21,57 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startNewRound()
+        setupSlider()
+        startNewGame()
     }
     
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
-        score += points
+        var points = 100 - difference
         
+        let title: String
+        if difference == 0 {
+            title = "You read my mind!\nMy number Was \(targetValue)"
+            points += 100
+        } else if difference < 5 {
+            title = "Nearly there!\nMy Number was \(targetValue)"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Close-ish.\nMy Number was \(targetValue)"
+        } else {
+            title = "Not even close...\n My number was \(targetValue)"
+        }
+        
+        score += points
         let message = "You scored \(points) points"
         
-        let alert = UIAlertController(
-            title: "My number is \(targetValue)",
-            message: message,
-            preferredStyle: .alert)
-        
-        let action = UIAlertAction(
-            title: "Try again!",
-            style: .default,
-            handler: nil)
-        
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "try your luck!", style: .default) { _ in
+            self.startNewRound()
+        })
+        present(alert, animated: true)
         
         startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = Int(slider.value)
-        sLabel.text = "\(currentValue)" // displays the current number on the slider in real time
+        sLabel.text = "\(currentValue)"
+    }
+    
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+        addTransition()
     }
     
     func startNewRound() {
         round += 1
         targetValue = Int.random(in: 1...100)
-        currentValue = 1 // moves the slider back to 1 after each round
+        currentValue = 1
         slider.value = Float(currentValue)
         sLabel.text = "\(currentValue)"
         updateLabels()
@@ -64,5 +80,31 @@ class ViewController: UIViewController {
     func updateLabels() {
         scoreLabel.text = String(score)
         roundLabel.text = String(round)
+    }
+    
+    private func setupSlider() {
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+    }
+    
+    private func addTransition() {
+        let transition = CATransition()
+        transition.type = .fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        view.layer.add(transition, forKey: nil)
     }
 }
